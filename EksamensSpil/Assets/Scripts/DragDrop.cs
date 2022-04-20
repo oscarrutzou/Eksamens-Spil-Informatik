84cfbdbaefe9d92163556b3b4c4f0f1b0f9246ea
oscarrutzou <91065000+oscarrutzou@users.noreply.github.com>
2022-04-20 13:03:41 +0200

    De fleste ting er kommet ind
    UI i Display mangler

---
+++ EksamensSpil/Assets/Scripts/DragDrop.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+
+    private float distanceFromCamera;
+    Rigidbody2D r;
+    public float speed;
+    Vector3 pos;
+
+    void Start()
+    {
+        distanceFromCamera = Vector3.Distance(this.gameObject.transform.position, Camera.main.transform.position);
+        r = this.gameObject.transform.GetComponent<Rigidbody2D>();
+    }
+    //Evt. brug eller slet
+
+
+    void OnMouseDrag()
+    {
+        pos = Input.mousePosition;
+        pos.z = distanceFromCamera;
+        pos = Camera.main.ScreenToWorldPoint(pos);
+        r.velocity = (pos - this.gameObject.transform.position) * 10;
+    }
+
+    void OnMouseUp()
+    {
+        r.velocity = Vector3.zero;
+
+        //For at få den til at flyve efter at være kastet
+        r.velocity = (pos - this.gameObject.transform.position) * speed;
+    }
+
+
+
+    //    //Virker ved venstre klik
+    //    if (Input.GetMouseButtonDown(0))
+
+
+}
---
+++ EksamensSpil/Assets/Scripts/TrashDelete.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashDelete : MonoBehaviour
+{
+
+    public float moveSpeed = 5;
+    //Til hvor meget score de hver objekter har.
+    public int score;
+    //Laver en variable så man kan kontakte "Display" scriptet.
+    private Display display;
+    public Rigidbody2D rb2D;
+    public float thrust = 1f;
+
+    //Den er i Awake, da den skal ske før "void Start."
+    void Awake()
+    {
+        //Tilslutter sig "Display" scriptet.
+        display = GameObject.FindObjectOfType<Display>();
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        //Fetch the Rigidbody from the GameObject with this script attached
+        // m_Rigidbody = GetComponent<Rigidbody>();
+    }
+    //Når den rammer noget, bliver metoden kaldt.
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Hvis den rammer en maskrine, bliver objektet slettet.
+        if (collision.collider.CompareTag("Machine"))
+        {
+            Destroy(this.gameObject);
+
+            //Sender scoren af objektet til "Display scriptet"
+            display.UpdateScore(score);
+
+
+            //Lav så den starter en animation når den dør.
+        }
+
+        if (collision.collider.CompareTag("Lava"))
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (collision.collider.CompareTag("ConveyorBelt"))
+        {
+            Debug.Log("test");
+            rb2D.velocity = new Vector2(0, 10);
+        }
+
+    }
+
+}
